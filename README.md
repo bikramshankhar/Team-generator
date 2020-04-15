3385b0686ddf69d12887cb8fed801d6ab94e22e5
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-04-15 00:06:26 -0500

    fixed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Team genenator 
-This Team generator command line application. The application will prompt the user for information about the team manager and then information about the team members. Node CLI will gather information about each employee and create team.html file.
+This is team generator command line application. The application will prompt the user for information about the team manager and then information about the team members. Node CLI will gather information about each employee and create team.html file.
+
+## Screenshot
+ <img src="./images/screenshot.png">
+ <img src="./images/screenshot2.png">
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const outputPath = path.resolve(__dirname, "output", "team.html");
+const Manager = require("./lib/Manager");
 
 const employees = [];
 
-function initApp() {
-        addMember();
+function init() {
+    writeHtml();
+    question();
 }
 
-function addMember() {
+function question() {
     inquirer.prompt([{
         message: "Enter team member's name",
         name: "name"
@@ -71,7 +69,7 @@ function addMember() {
             addHtml(newMember)
             .then(function() {
                 if (moreMembers === "yes") {
-                    addMember();
+                    question();
                 } else {
                     finishHtml();
                 }
@@ -81,5 +79,113 @@ function addMember() {
     });
 }
 
+function writeHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        <title>My Team</title>
+    </head>
+    <body>
+     <div class="jumbotron" style="background-color:#262626">
+        <h1 class="text-center text-white">My Team</h1>
+        </div>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
 
-initApp();
+function addHtml(member) {
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="card col-md-4 col-sm-12 mb-3 px-0">
+            <div class="card-body bg-dark text-white">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+            </div>
+            <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${ email }">${ email }</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${ gitHub }">${ gitHub }</a>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="card col-md-4 col-sm-12 mb-3 px-0">
+            <div class="card-body bg-dark text-white">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${ email }">${ email }</a></li>
+                    <li class="list-group-item">School: ${school}</li>
+                </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="card col-md-4 col-sm-12 mb-3 px-0">
+            <div class="card-body bg-dark text-white">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+            </div>
+            <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${ email }">${ email }</a></li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        console.log("add more");
+        fs.appendFile("./output/team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+init();
